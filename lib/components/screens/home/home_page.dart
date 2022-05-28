362f04ad5b425926c323d89786f45f28b930866a
saliou-t <zalltalla46@gmail.com>
2022-05-28 13:07:47 +0000

    ajout des assets, et mise en place du navbar

---
+++ lib/components/screens/home/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // elevation: 0,
+        // leading: IconButton(icon: SvgPicture.asset("assets/icones/menu.svg"),onPressed: (){},),
+      ),
+    );
+  }
+}
