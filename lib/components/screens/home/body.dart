01839c7a5834c4cb5d0882cdb0d849cd312df69b
saliou-t <zalltalla46@gmail.com>
2022-05-30 10:14:54 +0000

    detache title_with_btn_more

--- lib/components/screens/home/body.dart
+++ lib/components/screens/home/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:planapp/components/screens/home/header_with_search.dart';
-import 'package:planapp/constants.dart';
+import 'package:planapp/components/screens/home/title_with_more_btn.dart';
 // import 'package:planapp/constants.dart';
 
 class Body extends StatelessWidget {
@@ -13,49 +13,9 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Header(),
-          Row(
-            children: <Widget>[
-              TitleWithCustumLine(
-                text: "Recommandé pour vous"
-              )
-            ],
-          ),
+          TextWithBtnMore(title: 'jklm', press: (){}),
         ],
       ),
-    );
-  }
-}
-
-//vue qu'on utilise souvant le titre souligner, on crée une fonction pour éviter la rpétition.
-class TitleWithCustumLine extends StatelessWidget {
-  const TitleWithCustumLine(
-    {Key? key, required this.text,}
-    
-  ) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 24,
-            child: Stack(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: kDefaultPadding /4)
-                ),
-                Text(
-                  text, 
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),    
     );
   }
 }
