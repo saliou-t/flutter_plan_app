14a3fc0cc561753974a9d9dc5ed9ffbac7b64444
saliou-t <zalltalla46@gmail.com>
2022-05-30 17:16:08 +0000

    refactory ~scroll view~ des recommandations

--- lib/components/screens/home/body.dart
+++ lib/components/screens/home/body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:planapp/components/Recommanded.dart';
 import 'package:planapp/components/screens/home/header_with_search.dart';
-import 'package:planapp/components/screens/home/plante_recommended.dart';
 import 'package:planapp/components/screens/home/title_with_more_btn.dart';
-import 'package:planapp/constants.dart';
 // import 'package:planapp/constants.dart';
 
 class Body extends StatelessWidget {
@@ -16,16 +15,7 @@ class Body extends StatelessWidget {
         children: <Widget>[
           Header(),
           TextWithBtnMore(title: 'Nos recomandations', press: (){}),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child:Row(
-              children: <Widget>[
-                PlantRecommanded(size: size, contry: 'Sénégal', price: '\$500', titleImage: 'Santanara', imagePath: '/home/cheikh/Bureau/udemy/planapp/assets/images/planta1.jpg', press: (){},),
-                PlantRecommanded(size: size, contry: 'Sénégal', price: '\$500', titleImage: 'Santanara', imagePath: '/home/cheikh/Bureau/udemy/planapp/assets/images/planta1.jpg', press: (){},),
-                PlantRecommanded(size: size, contry: 'Sénégal', price: '\$500', titleImage: 'Santanara', imagePath: '/home/cheikh/Bureau/udemy/planapp/assets/images/planta1.jpg', press: (){},),
-              ],
-            ) ,
-          )  
+          Recomanded(size: size)  
           
         ],
       ),
--- lib/components/screens/home/plante_recommended.dart
+++ lib/components/screens/home/plante_recommended.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planapp/components/screens/home/plante_scroller.dart';
 import 'package:planapp/constants.dart';
 
 class PlantRecommanded extends StatelessWidget {
@@ -19,48 +20,11 @@ class PlantRecommanded extends StatelessWidget {
     return Container(
       margin: EdgeInsets.only(left: kDefaultPadding, top: kDefaultPadding),
       width: size.width * 0.4, //ceci prend 40% du width global
+      height: size.height * 0.6,
       child: Column(
         children: <Widget>[
           Image.asset(imagePath),
-          GestureDetector(
-            onTap: press(),
-            child: Container(
-              padding: EdgeInsets.all(kDefaultPadding / 2),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(12),
-                    bottomRight: Radius.circular(12),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(10, -10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.95))
-                  ]),
-              child: Row(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(children: [
-                      TextSpan(
-                          text: titleImage + "\n".toUpperCase(),
-                          style: Theme.of(context).textTheme.button),
-                      TextSpan(
-                          text: contry, style: TextStyle(color: kPrimaryColor))
-                    ]),
-                  ),
-                  Spacer(),
-                  Text(
-                    price,
-                    style: Theme.of(context)
-                        .textTheme
-                        .button!
-                        .copyWith(color: kPrimaryColor),
-                  )
-                ],
-              ),
-            ),
-          )
+          PlanteScroll(press: press, titleImage: titleImage, contry: contry, price: price)
         ],
       ),
     );
